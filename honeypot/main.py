0d6dd7c11692506724019e0ee136b0be85463879
nlouis56 <nataniel-.louis@epitech.eu>
2025-04-18 13:29:10 +0200

    error management

--- honeypot/main.py
+++ honeypot/main.py
@@ -60,10 +60,16 @@ MOTD = [
 class HoneypotDatabase:
     def __init__(self):
         dbname=os.environ.get("DB_NAME", "honeypot"),
+        dbname = dbname[0] if isinstance(dbname, tuple) else dbname
         user=os.environ.get("DB_USER", "user"),
+        user = user[0] if isinstance(user, tuple) else user
         password=os.environ.get("DB_PASSWORD", "password"),
+        password = password[0] if isinstance(password, tuple) else password
         host=os.environ.get("DB_HOST", "localhost"),
+        host = host[0] if isinstance(host, tuple) else host
         port=os.environ.get("DB_PORT", "5432")
+        port = port[0] if isinstance(port, tuple) else port
+        port = int(port) if port.isdigit() else 5432
 
         print("Trying to connect with credentials:")
         print(dbname)
